278ff30e089a655057ceb2ecb49e7a492e8e9a50
Ольга Богданова <bon@incom-sr.local>
2023-08-15 16:33:24 +0300

    refactoring script error

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const btn = document.querySelector("#btn");
+window.addEventListener('DOMContentLoaded', ()=>{
+  const btn = document.querySelector("#btn");
+  console.log(btn);
 const sideBar = document.querySelector(".sidebar");
 btn.addEventListener("click", () => {
   sideBar.classList.toggle("active");
 });
+
+})
